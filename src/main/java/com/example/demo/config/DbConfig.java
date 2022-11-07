4b61335347a06fd7ff12822819655af2c23fdba4
Kevin Krisna <kevinkrisnadp98@gmail.com>
2022-11-07 17:41:22 +0700

    add db config

---
+++ src/main/java/com/example/demo/config/DbConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Primary
+    @Bean(name = "dbDataSource")
+    public DataSource dbDataSource() {
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url");
+        dataSource.setUsername(env.getProperty("spring.datasource.username");
+        dataSource.setPassword(env.getProperty("spring.datasource.password");
+
+        return dataSource;
+    }
+}
