534a6704a89622b9f6a4d72851f8361449b39c88
Kevin Krisna <kevinkrisnadp98@gmail.com>
2022-11-18 13:43:35 +0700

    test

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:latest
 EXPOSE 8080
-COPY target/demo-0.0.1-SNAPSHOT.jar .
+#COPY target/demo-0.0.1-SNAPSHOT.jar .
 COPY config/logback.xml .
-ENTRYPOINT java -jar -server -Xms4G -Xmx16G demo-0.0.1-SNAPSHOT.jar
+ADD target/*.jar app.jar
+ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/com/example/demo/config/DbConfig.java
+++ src/main/java/com/example/demo/config/DbConfig.java
@@ -22,7 +22,7 @@ public class DbConfig {
     public DataSource dbDataSource() {
         log.info("env: {} ",env.getProperty("spring.datasource.driver-class-name"));
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driverorg.postgresql.Driver");
+        dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl(System.getenv("DB_URL") !=null ? System.getenv("DB_URL") : env.getProperty("spring.datasource.url"));
         dataSource.setUsername(System.getenv("DB_USERNAME") != null ? System.getenv("DB_USERNAME") : env.getProperty("spring.datasource.username"));
         dataSource.setPassword(System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : env.getProperty("spring.datasource.password"));
