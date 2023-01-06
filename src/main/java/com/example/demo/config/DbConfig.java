db112e14ddab90c875ae863d1f3f12b58c320f90
Astraizer <kevinkrisnadp98@gmail.com>
2023-01-06 08:44:18 +0000

    test build

--- src/main/java/com/example/demo/config/DbConfig.java
+++ src/main/java/com/example/demo/config/DbConfig.java
@@ -20,7 +20,6 @@ public class DbConfig {
     @Primary
     @Bean(name = "dbDataSource")
     public DataSource dbDataSource() {
-        log.info("env: {} ",env.getProperty("spring.datasource.driver-class-name"));
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl(System.getenv("DB_URL") !=null ? System.getenv("DB_URL") : env.getProperty("spring.datasource.url"));
